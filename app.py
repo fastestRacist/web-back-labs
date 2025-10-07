0a92056545dd2261df930f24f397eab3896b0e4d
Хобенков Егор <khobenkov@yandex.ru>
2025-10-07 16:27:17 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -301,3 +301,6 @@ def example():
         {'name': 'манго', 'price': 321},
         ]
     return render_template('example.html', name = name, group = group, course = course, lab_num = lab_num, fruits = fruits)
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
