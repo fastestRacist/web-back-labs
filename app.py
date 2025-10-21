e9e35923949518c49e9d23d192184cc016ba9df6
Хобенков Егор <khobenkov@yandex.ru>
2025-10-21 18:52:43 +0700

    заготовка к 3 лабораторной работе

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, url_for, request, redirect, abort, render_template
 import datetime
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
+
 
 # @app.errorhandler(404)
 # def not_found(err):
@@ -26,7 +29,9 @@ app.register_blueprint(lab2)
 # </html>
 # ''', 404
 
+
 log_404 = []
+
 
 @app.errorhandler(404)
 def not_found(err):
@@ -83,6 +88,7 @@ def index():
     <ul>
         <li><a href="/lab1">Первая лабораторная</a></li>
         <li><a href="/lab2/">Вторая лабораторная</a></li>
+        <li><a href="/lab3/">Третья лабораторная</a></li>
     </ul>
     <footer>
         <p>Хобенков Егор Алексеевич, ФБИ-32, 3 курс, 2025 год</p>
