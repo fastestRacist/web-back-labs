91cdc6d9b55ad25268f8b67eba3e23daea432784
Студент Факультета Бизнеса <student@FB.LOCAL>
2025-12-09 15:22:31 +0700

    доделать с 13 страницы

--- lab8.py
+++ lab8.py
@@ -4,6 +4,8 @@ from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 from os import path
+from db import db
+from db.models import users, articles
 
 from datetime import datetime
 from lab5 import db_close, db_connect
@@ -14,3 +16,12 @@ lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
 def lab():
     return render_template('lab8/lab8.html', login = session.get('login'))
+
+
+@lab8.route('/lab8/register/', method = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
