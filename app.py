2f50787487392ce384358cf85c1dd3e6fd68d140
Хобенков Егор <khobenkov@yandex.ru>
2025-12-16 16:29:51 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.login_view = 'lab8.login'
@@ -31,6 +32,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'gigasecret key')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
@@ -136,6 +138,7 @@ def index():
         <li><a href="/lab6/">Шестая лабораторная</a></li>
         <li><a href="/lab7/">Седьмая лабораторная</a></li>
         <li><a href="/lab8/">Восьмая лабораторная</a></li>
+        <li><a href="/lab9/">Девятая лабораторная</a></li>
     </ul>
     <footer>
         <p>Хобенков Егор Алексеевич, ФБИ-32, 3 курс, 2025 год</p>
